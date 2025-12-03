87cb63e0d5846fd35f20686398236eb827ee915e
Viktor Minin <viktorminin@MacBook.local>
2025-12-03 10:55:16 +0200

    Configure Cloudinary for persistent media storage

--- peertopeer/settings.py
+++ peertopeer/settings.py
@@ -30,6 +30,8 @@ INSTALLED_APPS = [
     'core',
     'news',
     'ckeditor',
+    'cloudinary_storage',
+    'cloudinary',
 ]
 
 MIDDLEWARE = [
@@ -117,6 +119,15 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
+# Cloudinary Configuration
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Email Configuration
 if DEBUG:
